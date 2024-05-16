759bd66695ff482233c50ed75ed1e53e2b690f49
hindujha-vijayakumar <hindujhavijayakumar@gmail.com>
2024-05-16 18:33:57 +0530

    Update Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,7 @@
-module encoder_8(d,a,b,c);
-input [7:0]d;
+module encoder_8(i, a, b, c);
+input [7:0]i;
 output a,b,c;
-
-
-
+assign a=i[4] | i[5] | i[6] | i[7];
+assign b=i[2] | i[3] | i[6] | i[7];
+assign c=i[1] | i[3] | i[5] | i[7];
 endmodule
