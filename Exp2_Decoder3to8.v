d0bec98b8a040b40941315b6f750a17ec4067aee
hindujha-vijayakumar <hindujhavijayakumar@gmail.com>
2024-05-16 18:34:36 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,12 @@
-module decoder_8(a,b,c,y);
-input a,b,c;
-output[7:0]y;
-
-
-
+module decoder_8(s,y);
+input [2:0]s;
+output [7:0]y;
+assign y[0]=~s[2]&~s[1]&~s[0];
+assign y[1]=~s[2]&~s[1]&s[0];
+assign y[2]=~s[2]&s[1]&~s[0];
+assign y[3]=~s[2]&s[1]&s[0];
+assign y[4]=s[2]&~s[1]&~s[0];
+assign y[5]=s[2]&~s[1]&s[0];
+assign y[6]=s[2]&s[1]&~s[0];
+assign y[7]=s[2]&s[1]&s[0];
 endmodule
