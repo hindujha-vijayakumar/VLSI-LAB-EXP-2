34b20cdd78b857bbd00a8e1c4a50bec953e21073
hindujha-vijayakumar <hindujhavijayakumar@gmail.com>
2024-05-16 18:38:49 +0530

    Update Exp2_Magnitudecomparator.v

--- Exp2_Magnitudecomparator.v
+++ Exp2_Magnitudecomparator.v
@@ -1,10 +1,25 @@
-Module comparator(a,b,eq,lt,gt);
-input [3:0] a,b;
-output reg eq,lt,gt;
-always @(a,b)
+module comparator(a,b,l,g,e);
+input [3:0]a,b;
+output reg l,g,e;
+always@(*)
 begin
- 
- 
- 
-end 
+if(a>b)
+begin
+l=1'b0;
+g=1'b1;
+e=1'b0;
+end
+else if(a<b)
+begin
+l=1'b1;
+g=1'b0;
+e=1'b0;
+end
+else
+begin
+l=1'b0;
+g=1'b0;
+e=1'b1;
+end
+end
 endmodule
