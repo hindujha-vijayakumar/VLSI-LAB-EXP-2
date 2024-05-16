a2f0e05362ea6667513d44712d1035690c89835c
hindujha-vijayakumar <hindujhavijayakumar@gmail.com>
2024-05-16 18:40:09 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,9 +1,10 @@
-module demux_8(s,d,y);
-input [2:0]s;
-input d;
-output [7:0]y;
-
-
-
-
+module demux_8(a,b,c,sum,carry);
+input a,b,c;
+output sum, carry;
+wire w1,w2,w3;
+xor gl(wl,a,b);
+xor g2 (w2,a,b);
+xor g3 (sum, w1,c);
+and (w3,c,w1);
+or g5 (carry, w3,w2);
 endmodule
